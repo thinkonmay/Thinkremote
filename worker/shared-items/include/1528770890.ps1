fe322f1981f7e9d435937ba5bd0f067881a6d9dc
pigeatgarlic <64737125+pigeatgarlic@users.noreply.github.com>
2021-12-18 16:29:02 +0700

    🧹 clean file

--- worker/shared-items/include/1528770890.ps1
+++
@@ -1,3 +0,0 @@
-$CpuCores = (Get-WMIObject Win32_ComputerSystem).NumberOfLogicalProcessors
-$Samples = (Get-Counter "\Process($Processname*)\% Processor Time").CounterSamples
-$Samples | ConvertTo-Json
