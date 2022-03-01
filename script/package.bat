a27e584ce7ae5344f2f2250bb0b537a5ec023c35
pigeatgarlic <64737125+pigeatgarlic@users.noreply.github.com>
2022-03-02 06:05:15 +0700

    format shared items

--- script/package.bat
+++ script/package.bat
@@ -5,11 +5,10 @@ dotnet build signalling --output "bin" --self-contained false
 call "C:\Program Files\Microsoft Visual Studio\2022\Preview\VC\Auxiliary\Build\vcvarsall.bat" amd64_x86
 
 
-RMDIR /S /Q build && mkdir build
+RMDIR /S /Q build 
+mkdir build
 
-cd build && cmake .. && msbuild ALL_BUILD.vcxproj 
-cd ..
-cd build && msbuild ALL_BUILD.vcxproj && cd ..
+cd build && cmake .. && msbuild ALL_BUILD.vcxproj && cd ..
 
 robocopy script bin /E
 robocopy deployment/lib bin /E
