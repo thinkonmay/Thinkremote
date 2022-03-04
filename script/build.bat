65db15d64c455a637a9ec0648cbd4a96bbfed4fa
epitchi <thienvanlea1@gmail.com>
2022-03-04 11:33:08 +0700

    Update: Add static file for webapp

--- script/build.bat
+++ script/build.bat
@@ -1,6 +1,7 @@
 rmdir bin
 dotnet build worker/port-forward --output "bin" 
 dotnet build signalling --output "bin"
+dotnet build client/webapp --output "bin"
 call "C:\Program Files\Microsoft Visual Studio\2022\Preview\VC\Auxiliary\Build\vcvarsall.bat" amd64_x86
 cd build
 cmake ..
